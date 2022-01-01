a05401c3e9261dec57a602efd3ffa6b2d34c0f47
developerOlive <chloehiho@gmail.com>
2022-01-01 20:25:51 +0900

    Initial commit

---
+++ src/main/java/com/java_stream_api/JavaStreamApiApplication.java
@@ -0,0 +1,13 @@
+package com.java_stream_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaStreamApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaStreamApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java_stream_api/JavaStreamApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java_stream_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStreamApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
